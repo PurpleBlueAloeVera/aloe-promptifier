e7b4eb7c030dab2dc34c4d237d3a840ce0618563
PurpleBlueAloeVera <135618831+PurpleBlueAloeVera@users.noreply.github.com>
2023-08-16 12:49:04 +0200

    Update aloe-promptifier.py

--- scripts/aloe-promptifier.py
+++ scripts/aloe-promptifier.py
@@ -41,7 +41,7 @@ class Script(scripts.Script):
             "In addition, you're gonna want to type whatever LoRAs or embeddings you want the triggers words to add to the prompt. In the triggers you'll want to put whatever keyword(s) you want to be the triggers that'll append the addition you chose to your prompt. Then, by selecting POS or NEG, you decide to lock its place. Meaning if you picked POS, where ever you type the trigger, will only append the addition to the positive prompt. And vice-vera. You may also choose NONE, if you want it to behave based on where your keyword is detected. If detected in positive prompt, addition will be appended to positive prompt."
         )
 
-        main_accordion = gr.Accordion("Aloe's Promptifier", open=True)
+        main_accordion = gr.Accordion("Aloe's Promptifier", open=False)
 
         with main_accordion:
             addition_input = gr.Textbox(label="Addition", placeholder="Type whatever LoRAs, embeddings.. or plain text that you want the triggers words to add to the prompt")
@@ -70,22 +70,24 @@ class Script(scripts.Script):
             detected_additions_main = set()
             detected_additions_negative = set()
 
-            for addition, info in additions_data.items():
-                for prompt in p.all_prompts:
+            # Function to detect triggers and add to detected additions set
+            def detect_and_add(prompt, additions_set):
+                for addition, info in additions_data.items():
                     for trigger in info["triggers"]:
                         if re.search(r'\b' + re.escape(trigger) + r'\b', prompt):
                             if info["type"] == "[pos]":
-                                detected_additions_main.add(addition)
+                                additions_set.add(addition)
                             elif info["type"] == "[neg]":
                                 detected_additions_negative.add(addition)
                             elif info["type"] == "None":
-                                detected_additions_main.add(addition)  # If trigger is in a positive prompt
-                                # If you also want to check the negative prompts when type is "None", 
-                                # you'll need to loop over them similarly as for the positive prompts
-                                for neg_prompt in p.all_negative_prompts:
-                                    if re.search(r'\b' + re.escape(trigger) + r'\b', neg_prompt):
-                                        detected_additions_negative.add(addition)  # If trigger is in a negative prompt
+                                additions_set.add(addition)
                             break
+            
+            for prompt in p.all_prompts:
+                detect_and_add(prompt, detected_additions_main)
+
+            for neg_prompt in p.all_negative_prompts:
+                detect_and_add(neg_prompt, detected_additions_negative)
 
             for addition in detected_additions_main:
                 p.all_prompts = [prompt + addition for prompt in p.all_prompts]
@@ -99,7 +101,6 @@ class Script(scripts.Script):
                 p.extra_generation_params["Trigger words prompt"] = original_prompt
         else:
             print(f"File {additions_file} not found.", file=sys.stderr)
-
 
 
     def append_text(self, p, addition):
