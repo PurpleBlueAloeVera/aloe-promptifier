31ad1fa350aa5c39b7e03f115bd920b78c2e9eb7
PurpleBlueAloeVera <135618831+PurpleBlueAloeVera@users.noreply.github.com>
2023-08-17 22:09:42 +0200

    Update aloe-promptifier.py
    ## Safe Regulator
    
    If you host a Stable Diffusion bot, you can use the regulated.json to type safe words that will in turn make it impossible for people to use a certain amount of words based on your safewords. 
    
    Example:
    
    {
        "chibi|chib|kid|child|young|girl": ["nsfw", "erotica", "nudity"]
    }
    
    Add any number of safe-words you want in the first section, separated by vertical bars | and in the second section, you can add the number of banned words you want. If safe word in prompt = banned words don't make it to generation.
    
    ## Enable/Disable checkbox
    
    ## Make sure to save your .json files when updating. Make copies, just to be sure !

--- scripts/aloe-promptifier.py
+++ scripts/aloe-promptifier.py
@@ -3,6 +3,7 @@ import re
 import json
 import gradio as gr
 import modules.scripts as scripts
+import sys
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 repo_dir = os.path.join(current_dir, '..')
@@ -44,22 +45,57 @@ class Script(scripts.Script):
         main_accordion = gr.Accordion("Aloe's Promptifier", open=False)
 
         with main_accordion:
+            extension_toggle = gr.Checkbox(label="Enable Extension", default=False)
             addition_input = gr.Textbox(label="Addition", placeholder="Type whatever LoRAs, embeddings.. or plain text that you want the triggers words to add to the prompt")
             triggers_input = gr.Textbox(label="Trigger Words", placeholder="Type your trigger words for that specific addition. Write them separated by commas.")
             type_selector = gr.Radio(label="Type of Trigger", choices=["[pos]", "[neg]", "None"], default="None")  # Adding the type selector
             save_button = gr.Button(value="Save")
             help_text = gr.Textbox(label="Scroll down to see the full text", value=(help_value), editable=False, height=100, lines=8)
                 
-            save_button.click(self.output_func, inputs=[addition_input, triggers_input, type_selector], outputs=[addition_input, triggers_input])  # Add type_selector to inputs
+            save_button.click(self.output_func, inputs=[addition_input, triggers_input, type_selector, extension_toggle], outputs=[addition_input, triggers_input])
 
             # Return components as a list
-            return [addition_input, triggers_input, type_selector, save_button, help_text]
+            return [addition_input, triggers_input, type_selector, save_button, help_text, extension_toggle]
 
-    def output_func(self, addition, triggers, type_flag):
-        self.save_to_file(addition, triggers, type_flag)
-        return "", "", "Saved successfully!"  # Clearing the text boxes and updating the message box
+    def output_func(self, addition, triggers, type_flag, is_enabled):
+        if is_enabled:
+            self.save_to_file(addition, triggers, type_flag)
+            return "", "", "Saved successfully!"
+        return addition, triggers, "Extension disabled!"
 
-    def process(self, p, *args, **kwargs):
+    def regulator(self, p):
+        regulated_file = os.path.join(repo_dir, "regulated.json")
+        if os.path.exists(regulated_file):
+            with open(regulated_file, encoding="utf8") as f:
+                regulated_data = json.load(f)
+
+            for safe_words, banned_words in regulated_data.items():
+                safe_words_list = safe_words.split('|')
+                for safe_word in safe_words_list:
+                    # If any safe word is detected in the prompts
+                    if any(safe_word in prompt for prompt in p.all_prompts):
+                        def remove_banned_words(prompt):
+                            for word in banned_words:
+                                # Removing the banned word from the prompt using regex
+                                prompt = re.sub(r'\b' + re.escape(word) + r'\b', '', prompt)
+                            return prompt.strip()  # Removing any extra spaces
+
+                        # Apply the removal function to the prompts
+                        p.all_prompts = [remove_banned_words(prompt) for prompt in p.all_prompts]
+                        
+                        # If 'all_hr_prompts' attribute exists, do the same for them
+                        if getattr(p, 'all_hr_prompts', None) is not None:
+                            p.all_hr_prompts = [remove_banned_words(prompt) for prompt in p.all_hr_prompts]
+
+                        # Apply the removal function to negative prompts
+                        p.all_negative_prompts = [remove_banned_words(prompt) for prompt in p.all_negative_prompts]
+        else:
+            print(f"File {regulated_file} not found.", file=sys.stderr)
+
+
+    def process(self, p, is_enabled, *args, **kwargs):
+        if not is_enabled:
+            return
         additions_file = os.path.join(repo_dir, "additions_prompt.json")
         if os.path.exists(additions_file):
             with open(additions_file, encoding="utf8") as f:
@@ -68,7 +104,6 @@ class Script(scripts.Script):
             detected_additions_pos = set()
             detected_additions_neg = set()
 
-            # Function to detect triggers and add to detected additions set
             def detect_and_add(prompt, type_flag, detected_additions):
                 for addition, info in additions_data.items():
                     for trigger in info["triggers"]:
@@ -76,22 +111,16 @@ class Script(scripts.Script):
                             if info["type"] == type_flag:
                                 detected_additions.add(addition)
 
-            # Detect additions for positive prompts
-            # Detect additions for positive prompts
             for prompt in p.all_prompts:
                 detect_and_add(prompt, "None", detected_additions_pos)
                 detect_and_add(prompt, "[pos]", detected_additions_pos)
-                # Detect [neg] triggers in positive prompts but add to negative additions
                 detect_and_add(prompt, "[neg]", detected_additions_neg)
 
-            # Detect additions for negative prompts
             for prompt in p.all_negative_prompts:
                 detect_and_add(prompt, "None", detected_additions_neg)
                 detect_and_add(prompt, "[neg]", detected_additions_neg)
                 detect_and_add(prompt, "[pos]", detected_additions_pos)
 
-
-            # Appending the detected additions to the respective prompts
             for addition in detected_additions_pos:
                 p.all_prompts = [prompt + addition for prompt in p.all_prompts]
                 if getattr(p, 'all_hr_prompts', None) is not None:
@@ -101,6 +130,9 @@ class Script(scripts.Script):
                 p.all_negative_prompts = [prompt + addition for prompt in p.all_negative_prompts]
         else:
             print(f"File {additions_file} not found.", file=sys.stderr)
+
+        # Call the regulator function after processing with the additions
+        self.regulator(p)
 
 
 
